bdafe8f1782dc3903f61a782edd0bd57039eaa12
Lucca Anderle <lucca.anderle1512@gmail.com>
2025-06-10 14:09:47 -0300

    Página quartos.php completa

---
+++ quartos.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quartos</title>
+</head>
+<body>
+    <h1>Quartos</h1>
+
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="cadastro.php">Página de Cadastros</a>
+        <a href="logout.php">Logout</a>
+    </nav>
+
+    <?php
+        require_once 'require/conexao.php';
+        require_once 'require/functions.php';
+
+        verificar_codigo();
+        $conn = conectar_banco();
+
+        $sql = "SELECT * FROM quartos";
+
+        $result = mysqli_query($conn, $sql);
+
+        if(mysqli_affected_rows($conn) <= 0) {
+            exit("<h3>Não existem quartos cadastrados</h3>");
+        }
+
+        echo "<h3>Lista de Quartos</h3>";
+        echo    "<table>";
+        echo        "<tr>";
+        echo            "<th>Id</th>";
+        echo            "<th>Número</th>";
+        echo            "<th>Tipo</th>";
+        echo            "<th>Preço</th>";
+        echo            "<th>Disponibilidade</th>";
+        echo        "</tr>";
+        while($quarto = mysqli_fetch_assoc($result)) {
+            $id_quarto          = $quarto['id_quarto'];
+            $numero_quarto      = $quarto['numero'];
+            $tipo_quarto        = $quarto['tipo'];
+            $preco_quarto       = $quarto['preco'];
+            if($quarto['disponivel'] == 0) {
+                $disponivel_quarto = "Ocupado";
+            } else {
+                $disponivel_quarto = "Disponível";
+            }
+
+            echo "<tr>";
+            echo    "<td> $id_quarto </td>"; 
+            echo    "<td> $numero_quarto </td>"; 
+            echo    "<td> $tipo_quarto </td>"; 
+            echo    "<td> $preco_quarto </td>"; 
+            echo    "<td> $disponivel_quarto </td>"; 
+            echo "</tr>";
+        }
+        echo "</table>";
+
+    ?>
+</body>
+</html>
