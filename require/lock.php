637fdf24ca5bcc70e0aa9717100ac9f97e9a3133
MatheusMachadoDM <165315314+MatheusMachadoDM@users.noreply.github.com>
2025-06-12 20:38:47 -0300

    Update lock.php

--- require/lock.php
+++ require/lock.php
@@ -1,4 +1,5 @@
 <?php session_start();
+    //Verfica se as variaveis de sessão estão definidas
     if(!isset($_SESSION['email']) || !isset($_SESSION['senha'])) {
         header('location:index.php?codigo=0'); 
     }
